6b037a6fa400e255799380bc6550acbc69064218
Rxghavdev <mattaraghav67@gmail.com>
2024-10-17 12:55:32 +0530

    added redis cloud

--- BE/helpers/redisClient.js
+++ BE/helpers/redisClient.js
@@ -1,17 +1,27 @@
-const { createClient } = require("redis");
+const { createClient } = require('redis');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
 
 const redisClient = createClient({
+  password: process.env.REDIS_PASSWORD || '5rE5IRFytuBTd4KSpo9fe90O1SfchrN6',
   socket: {
-    host: process.env.REDIS_HOST || "localhost",
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT || 6379,
   },
 });
 
-redisClient.on("connect", () => console.log("Connected to Redis"));
-redisClient.on("error", (err) => console.error("Redis error:", err));
+// Event listeners for Redis connection
+redisClient.on('connect', () => console.log('Connected to Redis'));
+redisClient.on('error', (err) => console.error('Redis error:', err));
 
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+  }
 })();
 
 module.exports = redisClient;
